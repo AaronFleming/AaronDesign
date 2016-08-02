53068b28616e09d53fa846b3e0d6ac6abc964cc6
troydo42 <hoangdov@gmail.com>
2016-08-02 12:49:23 -0700

    Add iOS project

---
+++ TheatreBud/Pods/Target Support Files/Pods-TheatreBud/Pods-TheatreBud-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_TheatreBudVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_TheatreBudVersionString[];
+
---
+++ TheatreBud/Pods/Target Support Files/Pods-TheatreBudUITests/Pods-TheatreBudUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TheatreBudUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TheatreBudUITests
+@end
